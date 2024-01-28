03cf3362b5d3278b065812475f2e188ec02cf08a
Afif Ramadhan <macbookpro@Macbooks-MacBook-Pro.local>
2024-01-28 16:57:22 +0700

    animasi gambar project dan tema siang/malam

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleElement = document.querySelector('.toggle');
+const bodyElement = document.querySelector('body');
+
+toggleElement.addEventListener('click',function(){
+    if (bodyElement.classList.contains('siang')){
+        bodyElement.classList.remove('siang');
+        bodyElement.classList.add('malam');
+    }
+    else {
+        bodyElement.classList.remove('malam');
+        bodyElement.classList.add('siang');
+    }
+})
